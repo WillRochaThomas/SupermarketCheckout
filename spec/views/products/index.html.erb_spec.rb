8fd56a7d62de24aff01cc17392725093fa189a45
WillJThomas <jam3st0wn>
2012-08-10 20:40:08 +0100

    Product prices should be formatted when displayed, to always have two decimal places

--- spec/views/products/index.html.erb_spec.rb
+++ spec/views/products/index.html.erb_spec.rb
@@ -1,26 +1,44 @@
+#coding: utf-8
 require 'spec_helper'
 
 describe "products/index" do
-  before(:each) do
-    assign(:products, [
-      stub_model(Product,
-        :product_code => "Product Code",
-        :name => "Name",
-        :price => "9.99"
-      ),
-      stub_model(Product,
-        :product_code => "Product Code",
-        :name => "Name",
-        :price => "9.99"
-      )
-    ])
-  end
 
   it "renders a list of products" do
+    assign(:products, [
+        stub_model(Product,
+                   :product_code => "Product Code",
+                   :name => "Name",
+                   :price => "9.99"
+        ),
+        stub_model(Product,
+                   :product_code => "Product Code",
+                   :name => "Name",
+                   :price => "9.99"
+        )
+    ])
+
+    render
+
+    assert_select "tr>td", :text => "Product Code".to_s, :count => 2
+    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "tr>td", :text => "£9.99".to_s, :count => 2
+  end
+
+  it "it formats product prices with 2 decimal places even when decimal values are zero" do
+    assign(:products, [
+        stub_model(Product,
+                   :product_code => "Product Code",
+                   :name => "Name",
+                   :price => "£9.00"
+        )
+    ])
+
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "Product Code".to_s, :count => 2
     assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => "9.99".to_s, :count => 2
+    assert_select "tr>td", :text => "£9.00".to_s, :count => 2
   end
+
+
 end
--- spec/views/products/show.html.erb_spec.rb
+++ spec/views/products/show.html.erb_spec.rb
@@ -1,19 +1,35 @@
+#coding: utf-8
 require 'spec_helper'
 
 describe "products/show" do
-  before(:each) do
-    @product = assign(:product, stub_model(Product,
-      :product_code => "Product Code",
-      :name => "Name",
-      :price => "9.99"
-    ))
-  end
 
   it "renders attributes in <p>" do
+    @product = assign(:product, stub_model(Product,
+                                           :product_code => "Product Code",
+                                           :name => "Name",
+                                           :price => "9.99"
+    ))
+
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
+
     rendered.should match(/Product Code/)
     rendered.should match(/Name/)
-    rendered.should match(/9.99/)
+    rendered.should match(/£9.99/)
   end
+
+  it "it formats product prices with 2 decimal places even when decimal values are zero" do
+    @product = assign(:product, stub_model(Product,
+                                           :product_code => "Product Code",
+                                           :name => "Name",
+                                           :price => "9.00"
+    ))
+
+    render
+
+    rendered.should match(/Product Code/)
+    rendered.should match(/Name/)
+    rendered.should match(/£9.00/)
+  end
+
+
 end
